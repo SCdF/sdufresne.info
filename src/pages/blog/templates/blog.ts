1fb56acaa8e05e922c5695dc7755c2dacae66de7
Stefan du Fresne <stefan@sdufresne.info>
2025-11-26 10:36:21 +0000

    clean up blog parsing code

--- src/pages/blog/templates/blog.ts
+++ src/pages/blog/templates/blog.ts
@@ -7,3 +7,15 @@ type Frontmatter = {
 };
 
 export type BlogPost = MarkdownInstance<Frontmatter>;
+
+export const published = () =>
+  Object.values(import.meta.glob<BlogPost>("../*.md", { eager: true })).filter(
+    (post) => post.frontmatter.pubDate !== undefined
+  );
+
+export const latestFirst = (posts: BlogPost[]) =>
+  posts.sort(
+    (a, b) =>
+      new Date(b.frontmatter.pubDate || 0).getDate() -
+      new Date(a.frontmatter.pubDate || 0).getDate()
+  );
